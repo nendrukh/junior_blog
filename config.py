845d0e6b9d0d41aac35061acb0ccaa875ea88b04
Nikita <endrukh@gmail.com>
2024-03-15 16:37:20 +0300

    added readme, requirements and some changes in config

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+if not find_dotenv():
+    exit("Переменные окружения не загружены, так как нет файла .env")
+else:
+    load_dotenv()
+
 API_KEY = os.getenv("API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
